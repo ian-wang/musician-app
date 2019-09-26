937d5d4332a999a79de35e822c189799dc6cfa47
Ian Wang <ian.v3a.wang@gmail.com>
2019-09-26 15:44:41 +1000

    Add health check route

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,12 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+
+// Health check
+router.get('/health', (req, res) => {
+    res.status('200').send("status 200 - ok!");
+});
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
